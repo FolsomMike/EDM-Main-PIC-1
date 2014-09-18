d0b4361eebd2a9fc43d8a2fab0fe049e2b96cb17
Mike Schoonover <mike@folsomvillage.com>
2014-09-18 13:10:50 -0600

    MPLAB X project files after importing project from MPLAB 8.
    Signed-off-by: Mike Schoonover <mike@folsomvillage.com>

---
+++ OPT EDM Main PIC.X/build/default/production/_ext/1472/OPT EDM Main PIC 2.o.d
@@ -0,0 +1,5 @@
+build/default/production/_ext/1472/OPT\ EDM\ Main\ PIC\ 2.o:  \
+	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects\ Git\ Repositories/OPT\ EDM\ Main\ PIC/OPT\ EDM\ Main\ PIC\ 2.asm  \
+	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects\ Git\ Repositories/OPT\ EDM\ Main\ PIC/P16F648A.INC  \
+	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects\ Git\ Repositories/OPT\ EDM\ Main\ PIC/standard.mac  \
+
